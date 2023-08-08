8b4216771dd89ab9f707ad0a7e7b5aae32ad7345
Prativ <prativ.stha03@gail.com>
2023-08-08 14:53:13 +0545

    user profile and picture

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from .views import *
 
@@ -9,3 +11,7 @@ urlpatterns=[
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/',  auth_views.LogoutView.as_view(template_name='users/logout.html'),name="logout"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
