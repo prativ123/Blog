65ca221d99faf757ea66c9431caeab5f047ac4da
Prativ <prativ.stha03@gail.com>
2023-08-11 16:52:37 +0545

    Create, Update, and Delete Posts features added

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', about, name='blog-about'),
 ]
