285480f4980c33be3603e42122ab192c3324536e
Prativ <prativ.stha03@gail.com>
2023-09-26 21:43:12 +0545

    most viewed post and calender added

--- blogapp/templatetags/post_tags.py
+++ blogapp/templatetags/post_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Post
+from django.db.models import Sum
 
 register = template.Library()
 
@@ -8,4 +9,13 @@ register = template.Library()
 def show_latest_posts(count=3):
     latest_posts = Post.objects.order_by('-date_posted')[:count]
     return {'latest_posts': latest_posts}
+    pass
+
+@register.inclusion_tag('blogapp/most_viewed_posts.html')
+def show_most_viewed_posts(count=3):
+    most_viewed_posts = Post.objects.annotate(
+                                    total_views=Sum('blog_views')
+                                ).order_by('-total_views')[:count]
+                                
+    return {'most_viewed_posts' : most_viewed_posts}
     pass
